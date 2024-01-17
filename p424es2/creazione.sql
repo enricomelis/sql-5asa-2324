49886e2ee25c5ee9bf77410caff6ab66e54e315f
Enrico <enrico.melis.casa@gmail.com>
2024-01-17 17:32:42 +0100

    Update creazione.sql

--- p424es2/creazione.sql
+++ p424es2/creazione.sql
@@ -14,6 +14,6 @@ CREATE TABLE squadra (
     codice_caserma INT NOT NULL,
     nome_caserma VARCHAR(10) NOT NULL,
 
-    PRIMARY KEY (codice-squadra),
+    PRIMARY KEY (codice),
     FOREIGN KEY ( codice_caserma ) REFERENCES caserma.codice
 );
